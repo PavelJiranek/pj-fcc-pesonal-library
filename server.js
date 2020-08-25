fcb2144f1d6c5fedd3bb11431e01b63e54a00bed
Pavel Jiranek <jiranek35@gmail.com>
2020-08-25 16:22:36 +0200

    feat: helmet and nocache setup, add custom styles

--- server.js
+++ server.js
@@ -1,53 +1,65 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const nocache = require('nocache')
+require('dotenv').config();
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
-var app = express();
+const app = express();
+
+app.use(nocache());
+app.use(helmet({
+  hidePoweredBy: {
+    setTo: 'PHP 4.2.0',
+  },
+}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
-    .type('text')
-    .send('Not Found');
+      .type('text')
+      .send('Not Found');
 });
 
+const DEFAULT_PORT = 3000;
+
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
+app.listen(process.env.PORT || DEFAULT_PORT, function () {
+  console.log(`Listening on port ${process.env.PORT || DEFAULT_PORT}`);
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+      } catch (e) {
+        const error = e;
+        console.log('Tests are not valid:');
+        console.log(error);
       }
     }, 1500);
   }
